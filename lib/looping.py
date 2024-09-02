36ea66de4d61ac702518a7ffcebb92bb863355ed
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-02 21:25:38 +0300

    Defined function that loops over a range and prints out messages as per specified conditions

--- lib/looping.py
+++ lib/looping.py
@@ -15,8 +15,23 @@ happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
+  if len(int_list) == 0:
+    #return empty list to store squared integers
+    return []
+  else:
+    # return squared integers in a list
+    return [i**2 for i in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    
+    # define the range of numbers you want to loop over
+  for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+      print("FizzBuzz")
+    elif i % 3 == 0:
+      print("Fizz")
+    elif i % 5 == 0:
+      print("Buzz")
+    else:
+      print(i)
