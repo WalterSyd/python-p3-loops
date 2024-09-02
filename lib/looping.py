ccb8609f226e69d34e9492e6a345cf5e30ff3c19
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-02 20:49:06 +0300

    Defined a function that counts down seconds to new year and prints out message

--- lib/looping.py
+++ lib/looping.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    #apply & check countdown condition
+    i = 10
+    while i > 0:
+        print(i)
+        #Decrement the counter
+        i -= 1
+        #print message after countdown
+    print("Happy New Year!")
+happy_new_year()
 def square_integers(int_list):
     # code goes here!
     pass
